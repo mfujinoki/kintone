003d8a1c7fb181f217bf0d537cba4f4bc53a50ed
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2017-09-08 17:00:23 -0700

    Debug code

--- sample-google-calendar.js
+++ sample-google-calendar.js
@@ -19,24 +19,74 @@
     //レコード編集画面の保存成功後イベント及びレコード追加画面の保存成功後イベント
     kintone.events.on(['app.record.edit.submit.success','app.record.create.submit.success'],
         function(event) {
-          sendEvent(event.record);
-          return event;
+          var record = event.record;
+
+          //公開フラグが有効な時のみデータ送信
+          if(record.publish.value == '公開する')
+          {
+            // Google認証済みのチェック
+            if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
+              // Google認証の呼び出し
+              gapi.auth2.getAuthInstance().signIn();
+              alert('Google認証されていません。');
+              event.error = "Google認証されていません。";
+              return event;
+            }
+            // API リクエスト
+            // リクエストパラメータの設定
+            var params = {
+                // イベントのタイトル
+                'summary': record.event_name.value,
+                'start': {
+                    // 開始日・時刻
+                    'dateTime': record.start_datetime.value,
+                    'timezone': 'America/Los_Angeles'
+                },
+                'end': {
+                    // 終了日・時刻
+                    'dateTime': record.end_datetime.value,
+                    'timezone': 'America/Los_Angeles'
+                },
+                // 場所の指定
+                'location': record.event_location.value,
+                // イベントの説明
+                'description': record.event_description.value
+            };
+            // リクエストメソッドとパラメータを設定し、実行
+            gapi.client.calendar.events.insert(
+            {
+                    'calendarId': calendar_id,
+                    'resource': params
+            }).then(function(resp){
+                if(resp.error){
+                    event.error = "イベントの登録に失敗しました。";
+                }else{
+                      var body = {
+                      "app":kintone.app.getId(),
+                      "id":record.$id.value,
+                      "record":{
+                        "event_id":{
+                          "value":resp.result.id
+                        }
+                      }
+                    };
+                    return kintone.api(kintone.api.url('/k/v1/record',true),'PUT',body).then(function(success){
+                      alert("カレンダーにイベントを登録しました。");
+                      return event;
+                    }).catch(function(error){
+                      alert("Google イベントIDの登録に失敗しました。");
+                      event.erorr = "Google イベントIDの登録に失敗しました。";
+                      return event;
+                    });
+                }
+            });
+          }
     });
 
     // APIクライアントとOauth2モジュールのロード
     // モジュールロード後のinitClient関数の呼び出し
     gapi.load('client:auth2', initClient);
 
-    function sendEvent(record)
-    {
-      //公開フラグが有効な時のみデータ送信
-      if(record.publish.value == '公開する')
-      {
-          if(gapi.auth2.getAuthInstance().isSignedIn.get()){
-            makeApiCall(record);
-          }
-      }
-    }
     function initClient()
     {
         gapi.client.init({
@@ -50,46 +100,6 @@
               // Google認証の呼び出し
               gapi.auth2.getAuthInstance().signIn();
             }
-        });
-
-    }
-
-    function makeApiCall(record) {
-        // API リクエスト
-        // リクエストパラメータの設定
-        var event = {
-            // イベントのタイトル
-            'summary': record.event_name.value,
-            'start': {
-                // 開始日・時刻
-                'dateTime': record.start_datetime.value,
-                'timezone': 'America/Los_Angeles'
-            },
-            'end': {
-                // 終了日・時刻
-                'dateTime': record.end_datetime.value,
-                'timezone': 'America/Los_Angeles'
-            },
-            // 場所の指定
-            'location': record.event_location.value,
-            // イベントの説明
-            'description': record.event_description.value
-        };
-        // リクエストメソッドとパラメータを設定し、実行
-        gapi.client.calendar.events.insert(
-        {
-                'calendarId': calendar_id,
-                'resource': event
-        }).execute(function(resp){
-            if(resp.error){
-                alert("イベントの登録に失敗しました。");
-            }else{
-                record['event_id']['value'] = resp.id;
-                kintone.app.record.set(record);
-                alert("カレンダーにイベントを登録しました。");
-
-            }
-
         });
     }
 })();
