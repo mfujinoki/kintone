baf02dc3e2b45db834f0a1540e2d4370e086fbb1
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2017-09-11 17:02:15 -0700

    Debug

--- sample-google-calendar.js
+++ sample-google-calendar.js
@@ -38,12 +38,12 @@
     //レコード詳細画面の表示後イベント
     kintone.events.on('app.record.detail.show',function(event){
       // 増殖バグ回避
-      if (document.getElementById ('publish') !== null) {
+      if (document.getElementById ('publish_button') !== null) {
           return event;
       }
       // 画面下部にボタンを設置
       var publishButton = document.createElement('button');
-      publishButton.id = 'publish';
+      publishButton.id = 'publish_button';
       publishButton.innerHTML = '公開する';
       publishButton.className = "button-simple-cybozu geo-search-btn";
       publishButton.onclick = function() {
@@ -92,6 +92,9 @@
                     "record":{
                       "event_id":{
                         "value":resp.result.id
+                      },
+                      "publish":{
+                        "value":["公開済み"]
                       }
                     }
                   };
@@ -110,75 +113,4 @@
       kintone.app.record.getSpaceElement('publish_button_space').appendChild(publishButton);
       return event;
     });
-    //レコード編集画面の保存成功後イベント及びレコード追加画面の保存成功後イベント
-    /*kintone.events.on(['app.record.edit.submit.success','app.record.create.submit.success'],
-        function(event) {
-          var record = event.record;
-
-          //公開フラグが有効な時のみデータ送信
-          if(record.publish.value == '公開する')
-          {
-            // Google認証済みのチェック
-            if(!gapi.auth2.getAuthInstance().isSignedIn.get()){
-              // Google認証の呼び出し
-              gapi.auth2.getAuthInstance().signIn();
-              alert('Google認証されていません。');
-              event.error = "Google認証されていません。";
-              return event;
-            }
-            // API リクエスト
-            // リクエストパラメータの設定
-            var params = {
-                // イベントのタイトル
-                'summary': record.event_name.value,
-                'start': {
-                    // 開始日・時刻
-                    'dateTime': record.start_datetime.value,
-                    'timezone': 'America/Los_Angeles'
-                },
-                'end': {
-                    // 終了日・時刻
-                    'dateTime': record.end_datetime.value,
-                    'timezone': 'America/Los_Angeles'
-                },
-                // 場所の指定
-                'location': record.event_location.value,
-                // イベントの説明
-                'description': record.event_description.value
-            };
-            // リクエストメソッドとパラメータを設定し、実行
-            gapi.client.calendar.events.insert(
-            {
-                    'calendarId': calendar_id,
-                    'resource': params
-            }).then(function(resp){
-                if(resp.error){
-                    event.error = "イベントの登録に失敗しました。";
-                    return event;
-                }else{
-                      var body = {
-                      "app":kintone.app.getId(),
-                      "id":record.$id.value,
-                      "record":{
-                        "event_id":{
-                          "value":resp.result.id
-                        }
-                      }
-                    };
-                    return kintone.api(kintone.api.url('/k/v1/record',true),'PUT',body).then(function(success){
-                      alert("カレンダーにイベントを登録しました。");
-                      return event;
-                    }).catch(function(error){
-                      alert("Google イベントIDの登録に失敗しました。");
-                      event.erorr = "Google イベントIDの登録に失敗しました。";
-                      return event;
-                    });
-                }
-            },function(error){
-              alert("Google イベントIDの登録に失敗しました。");
-              return event;
-            });
-          }
-        return event;
-    });*/
 })();
