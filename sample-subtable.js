ef6d8a3e4be32330e4265f05e1aa745772395804
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-02-21 17:02:25 -0800

    Modify sample-subtable.js

--- sample-subtable.js
+++ sample-subtable.js
@@ -20,47 +20,42 @@
       kintone.api('/k/v1/records', 'GET', params).then(function(resp) {
           // success:生徒一覧を表示する
           var tableRecords = resp.records;
-          var studentTable = document.createElement('table');
-          studentTable.style.border = "1px solid black";
-          var caption = studentTable.createCaption();
-          caption.appendChild(document.createTextNode('生徒一覧'));
-          var tableHeader = studentTable.createTHead();
-          var tr = tableHeader.insertRow(-1);
-          tr.style.border = "1px solid black";
-          var th1 = document.createElement('th');
-          th1.style.border = "1px solid black";
-          var th2 = document.createElement('th');
-          th2.style.border = "1px solid black";
-          tr.appendChild(th1);
-          tr.appendChild(th2);
-
-          th1.appendChild(document.createTextNode('コード'));
-          th2.appendChild(document.createTextNode('氏名'));
-
-          studentTable.appendChild(tableHeader);
-
-          var body = document.createElement('tbody');
+          var studentTable = "<table class=\"subtable-gaia show-subtable-gaia\">";
+          studentTable += "<thead class=\"subtable-header-gaia\">";
+          studentTable += "<tr>";
+          studentTable += "<th class=\"subtable-label-gaia subtable-label-single_line_text-gaia\" style=\"width: 250px;\">";
+          studentTable += "<span class=\"subtable-label-inner-gaia\">";
+          studentTable += "コード";
+          studentTable += "</span>";
+          studentTable += "</th>";
+          studentTable += "<th class=\"subtable-label-gaia subtable-label-single_line_text-gaia\" style=\"width: 250px;\">";
+          studentTable += "<span class=\"subtable-label-inner-gaia\">";
+          studentTable += "氏名";
+          studentTable += "</span>";
+          studentTable += "</th>";
+          studentTable += "</tr>";
+          studentTable += "</thead>";
+          studentTable += "<tbody>";
           for (var i=0; i < tableRecords.length; i++)
           {
-            var row = body.insertRow(-1);
-            row.style.border = "1px solid black";
-            var cell1 = row.insertCell(-1);
-            cell1.style.border = "1px solid black"
-            var cell2 = row.insertCell(-1);
-            cell2.style.border = "1px solid black"
-
-            var link = document.createElement('a');
-            link.href = "/k/104/show#record=" + tableRecords[i].$id.value;
-            link.target = "_blank";
-            link.text = tableRecords[i].student_code.value;
-            cell1.appendChild(link);
-            cell2.appendChild(document.createTextNode(tableRecords[i].student_name.value));
-
+            studentTable += "<tr>";
+            studentTable += "<td>";
+            studentTable += "<div class=\"control-gaia control-single_line_text-field-gaia control-lookup-field-gaia control-show-gaia\">";
+            studentTable += "<a href=\"/k/104/show#record=" + tableRecords[i].$id.value + "\" target=\"_blank\">";
+            studentTable += "<span class=\"control-value-content-gaia\">" + tableRecords[i].student_code.value + "</span>";
+            studentTable += "</a>";
+            studentTable += "</div>"
+            studentTable += "</td>";
+            studentTable += "<td>";
+            studentTable += "<div class=\"control-gaia control-single_line_text-field-gaia control-show-gaia\">";
+            studentTable += "<span class=\"control-value-content-gaia\">" + tableRecords[i].student_name.value + "</span>";
+            studentTable += "</div>"
+            studentTable += "</td>";
+            studentTable += "</tr>";
           }
-
-          studentTable.appendChild(body);
-
-          subtableSpace.appendChild(studentTable);
+          studentTable += "</tbody>";
+          studentTable += "</table>";
+          subtableSpace.innerHTML = studentTable;
       }, function(error) {
           //error:エラーの場合はメッセージを表示する
           var errmsg = 'レコード取得時にエラーが発生しました。';
