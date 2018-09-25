7226c67c0fc6a8359184bad54306f57cfe95b073
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-09-24 17:05:31 -0700

    Create show_dat_of_week_plugin

--- plugins/show_day_of_week_plugin/js/desktop.js
+++ plugins/show_day_of_week_plugin/js/desktop.js
@@ -1,7 +1,5 @@
 /*
- * This sample code shows how to display error messages on kintone records.
- * In this particular example, the user must choose a value for the radio button field other than the default value
- * - otherwise an error will show up on the field and the record.
+ * This sample code displays the day of the week next to the Date field.
  * Copyright (c) 2018 Cybozu
  *
  * Licensed under the MIT License
@@ -16,30 +14,21 @@
         return false;
     }
 
-    var RADIOBUTTON = CONFIG.radio_button;
-    var RADIO_VALUE = CONFIG.default_value;
+    var DATE = CONFIG.date_field; // field code of date field
 
-    // Run code when saving a record
-    var ev = ['app.record.create.submit', 'app.record.index.edit.submit', 'app.record.edit.submit'];
-    kintone.events.on(ev, function(event) {
-
-        // Get record data
+    kintone.events.on(['app.record.detail.show'], function(event) {
         var record = event.record;
-        var selection = record[RADIOBUTTON].value;
-        var params = {
-            app: kintone.app.getId()
-        };
-        return kintone.api('/k/v1/app/form/fields', 'GET', params).then(function(resp) {
-            // Obtain label name of Radio button field
-            var name = resp.properties[RADIOBUTTON].label;
-            if (selection === RADIO_VALUE) {
-                var errMessage = "Please select other than \"" + selection + "\"";
-                // Show message underneath field
-                record[RADIOBUTTON].error = errMessage;
-                // Show message at top of record
-                event.error = errMessage + " for the field \"" + name + "\"";
-            }
-            return event;
-        });
+
+        var weekchars = CONFIG.name_of_days.split(',');
+        var date = new Date(record[DATE].value);
+        var day = weekchars[date.getUTCDay()];
+
+        var dayEl = document.createElement('span');
+        dayEl.textContent = ' (' + day + ')';
+
+        var dateEl = kintone.app.record.getFieldElement(DATE);
+        dateEl.appendChild(dayEl);
+
+        return event;
     });
 })(kintone.$PLUGIN_ID);
