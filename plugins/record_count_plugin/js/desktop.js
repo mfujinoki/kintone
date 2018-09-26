04382df5ea5105a15722c27967c55bf6ad979412
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-09-26 15:16:39 -0700

    Create record_count_plugin

---
+++ plugins/record_count_plugin/js/desktop.js
@@ -0,0 +1,40 @@
+/*
+ * This sample code calculates the total number of records with a given value,
+ * and displays the total at the top of the record list page.
+ * If the record list has a filter condition,
+ * the calculation will take in account of the filter condition.
+ * Copyright (c) 2018 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+(function(PLUGIN_ID) {
+    'use strict';
+
+    // Get plugin configuration settings
+    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+    // Get each settings
+    if (!CONFIG) {
+        return false;
+    }
+
+    var DROPDOWN = CONFIG.dropdown_field; //field code of dropdown field
+    var DROPDOWN_CHOICE1 = CONFIG.dropdown_choice; //name of dropdown choice
+
+    // Record List Event
+    kintone.events.on('app.record.index.show', function(event) {
+        // Gets records based on the current filter of the list
+        var query = kintone.app.getQueryCondition();
+        if (query === '') {
+            query += ' ' + DROPDOWN + ' in ("' + DROPDOWN_CHOICE1 + '")';
+        } else {
+            query += ' and ' + DROPDOWN + ' in ("' + DROPDOWN_CHOICE1 + '")';
+        }
+        kintone.api('/k/v1/records', 'GET', {
+            app: kintone.app.getId(),
+            query: query,
+            totalCount: true
+        }, function(resp) {
+            kintone.app.getHeaderMenuSpaceElement().innerHTML = DROPDOWN_CHOICE1 + '：' + resp.totalCount;
+        });
+    });
+})(kintone.$PLUGIN_ID);
