96fac2e31b3adc4a6c0a7cff3eb05d1927b70905
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-10-26 13:49:05 -0700

    Create date_from_now_plugin

---
+++ plugins/date_from_now_plugin/js/desktop.js
@@ -0,0 +1,32 @@
+/*
+ * This sample plugin converts the date into a new format in the selected locale.
+ * The new format is placed in the Record List instead of the original date format.
+ * Copyright (c) 2018 Cybozu
+ *
+ * Licensed under the MIT License
+ */
+(function(PLUGIN_ID) {
+    'use strict';
+
+    // Get plugin configuration settings
+    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+    // Get each settings
+    if (!CONFIG) {
+        return false;
+    }
+
+    moment.locale(CONFIG.locale);	//initialize the locale
+    var datetimefield = CONFIG.datetime_field;	//field code of a datetime field
+
+    // Set the Record List Event
+    kintone.events.on('app.record.index.show', function(e) {
+        // Get the datetime fields in the record list
+        var elements = kintone.app.getFieldElements(datetimefield);
+
+        // Display text formatted by Moment.js instead of the initial dates
+        for (var i = 0; i < e.records.length; i++) {
+            var date = e.records[i][datetimefield].value;
+            elements[i].innerText = moment(date).fromNow();
+        }
+    });
+})(kintone.$PLUGIN_ID);
